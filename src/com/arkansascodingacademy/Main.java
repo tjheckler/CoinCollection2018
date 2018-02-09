fddd40535ff29b3fc2f830adf6c9e54096c370f8
Louis <lrflrf@sbcglobal.net>
2018-02-09 10:49:07 -0600

    Initial check in

---
+++ src/com/arkansascodingacademy/Main.java
@@ -0,0 +1,84 @@
+package com.arkansascodingacademy;
+
+
+import java.math.BigDecimal;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Welcome to the ultimate coin collection program!");
+        System.out.println("First we will create a default coin collection with some exciting coins.");
+
+        final int quarterCount = 10;
+        final int dimeCount = 5;
+        final int nickelCount = 3;
+        final int pennyCount = 2;
+
+        /*CoinCollection coinCollection = new CoinCollection(quarterCount, dimeCount, nickelCount, pennyCount);
+
+        System.out.println("Let's take a look at the value of your new coin collection.");
+        System.out.println();
+
+        System.out.println("Coins have total face value of " + coinCollection.getFaceValue());
+        System.out.println("Coins have total collectible value of " + coinCollection.getCollectibleValue());
+
+        System.out.println();
+        System.out.println("Let's add some more coins to your collection.");
+
+        final int pennyYear1 = 1900;
+        final int nickelYear1 = 1903;
+        final int dimeYear1 = 1905;
+        final int quarterYear1 = 1908;
+
+        coinCollection.addPenny(pennyYear1);
+        coinCollection.addNickel(nickelYear1);
+        coinCollection.addDime(dimeYear1);
+        coinCollection.addQuarter(quarterYear1);
+
+        final int pennyYear2 = 1940;
+        final int nickelYear2 = 1993;
+        final int dimeYear2 = 1978;
+        final int quarterYear2 = 2008;
+
+        coinCollection.addPenny(pennyYear2);
+        coinCollection.addNickel(nickelYear2);
+        coinCollection.addDime(dimeYear2);
+        coinCollection.addQuarter(quarterYear2);
+
+        System.out.println("Let's take a look at the value of your improved coin collection.");
+        System.out.println();
+        System.out.println("Coins have total face value of " + coinCollection.getFaceValue());
+        System.out.println("Coins have total collectible value of " + coinCollection.getCollectibleValue());
+
+        System.out.println();
+        System.out.println("Let's take a look at the value of your improved coin collection in more detail.");
+        System.out.println();
+        System.out.println("Quarters have total face value of " + coinCollection.getQuartersFaceValue());
+        System.out.println("Quarters have total collectible value of " + coinCollection.getQuartersCollectibleValue());
+
+        System.out.println("Dimes have total face value of " + coinCollection.getDimesFaceValue());
+        System.out.println("Dimes have total collectible value of " + coinCollection.getDimesCollectibleValue());
+
+        System.out.println("Nickels have total face value of " + coinCollection.getNickelsFaceValue());
+        System.out.println("Nickels have total collectible value of " + coinCollection.getNickelsCollectibleValue());
+
+        System.out.println("Pennies have total face value of " + coinCollection.getPenniesFaceValue());
+        System.out.println("Pennies have total collectible value of " + coinCollection.getPenniesCollectibleValue());
+        */
+    }
+}
+
+/*
+Recommended order of modifiers
+public / private / protected
+abstract
+static
+final
+transient
+volatile
+**default**
+synchronized
+native
+strictfp
+ */
